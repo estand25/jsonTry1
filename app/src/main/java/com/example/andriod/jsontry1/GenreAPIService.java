017a26ef91d4428751761ecb34579eeccf57b45f
Standley Eugene <estand25@gmail.com>
2016-07-05 15:11:18 -0400

    Update GenreAPIService.java

--- app/src/main/java/com/example/andriod/jsontry1/GenreAPIService.java
+++ app/src/main/java/com/example/andriod/jsontry1/GenreAPIService.java
@@ -11,6 +11,6 @@ import retrofit2.http.GET;
  * Created by StandleyEugene on 7/5/2016.
  */
 public interface GenreAPIService {
-    @GET("movie/popular?api_key=d2919c2333e0f291ccf58c5e5ec88543") //THE_MOVIE_API_KEY - Is your key ;)
+    @GET("movie/popular?api_key=THE_MOVIE_API_KEY") //THE_MOVIE_API_KEY - Is your key ;)
     Call<JSONArray> readJsonArray();
 }
